3f7b257d13279f28c4c743993218a767cdb54d18
MohamedMaherAli <mohamedmhasanein@gmail.com>
2022-09-01 19:35:44 +0200

    fixed routes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='algorithm-visualizer'>
+    <BrowserRouter basename='http://MohamedMaherAli.github.io/algorithm-visualizer'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
